731e8d89ce934aa801b7d9efec53014c099dc4a8
Tigran Mogrov <tigranmogrov@gmail.com>
2024-01-31 15:18:26 +0200

    feat(server): add get products route

---
+++ server/api/products/index.get.ts
@@ -0,0 +1,27 @@
+import { H3Error, H3Event } from 'h3';
+import { PostLimitEnum } from '~~/enum/post.enum';
+import { Product } from '~~/server/api/products/product.model';
+import type { IProductsData } from '~~/types/product';
+
+export default defineEventHandler(async (event: H3Event): Promise<H3Error | IProductsData> => {
+  const query = getQuery(event);
+  const LIMIT = Number(query.limit) || PostLimitEnum.DEFAULT;
+  const { queryPrice, ...allQuery } = query;
+  const parsePrice = String(queryPrice).split('-');
+  const minPrice = Number(parsePrice[0]) || 0;
+  const maxPrice = Number(parsePrice[1]) || +Infinity;
+  try {
+    const products = await Product.find({ ...allQuery, price: { $gte: minPrice, $lte: maxPrice } });
+    const totalItems = await Product.countDocuments();
+    const totalPage = Math.ceil(totalItems / LIMIT);
+    return {
+      products,
+      totalItems,
+      totalPage,
+    };
+  } catch (error: any) {
+    return createError({
+      message: error.message,
+    });
+  }
+});
