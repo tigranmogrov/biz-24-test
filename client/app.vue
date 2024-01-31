f747ea8576fcfe44453fb07811aab89a19732273
Tigran Mogrov <tigranmogrov@gmail.com>
2024-01-31 14:45:25 +0200

    feat: add packges and start project

---
+++ client/app.vue
@@ -0,0 +1,16 @@
+<script lang="ts" setup></script>
+
+<template>
+  <div class="container px-4 mx-auto">
+    <nuxt-layout>
+      <nuxt-page />
+    </nuxt-layout>
+  </div>
+</template>
+
+<style>
+:root,
+body {
+  height: 100%;
+}
+</style>
---
+++ nuxt.config.ts
@@ -0,0 +1,39 @@
+import { resolve } from 'node:path';
+
+export default defineNuxtConfig({
+  devtools: { enabled: false },
+
+  modules: [
+    [
+      '@pinia/nuxt',
+      {
+        autoImports: ['acceptHMRUpdate', 'defineStore', ['defineStore']],
+      },
+    ],
+    '@nuxtjs/tailwindcss',
+    '@nuxt/image',
+  ],
+  runtimeConfig: {
+    mongodbUri: process.env.MONGODB_URI,
+    mongodbName: process.env.DATABASE_NAME,
+    userName: process.env.DATABASE_USER_NAME,
+  },
+  app: {
+    head: {
+      htmlAttrs: {
+        lang: 'en',
+        class: 'dark',
+      },
+      title: 'biz23 test',
+      charset: 'utf-8',
+    },
+  },
+  srcDir: './client',
+  serverDir: './server',
+  imports: {
+    dirs: ['stores', '~stores', '~node_modules', '~server', '~public'],
+  },
+  alias: {
+    cookie: resolve(__dirname, 'node_modules/cookie'),
+  },
+});
