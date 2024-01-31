411cb711448ca52123aa1d24f290109dc1bc6685
Tigran Mogrov <tigranmogrov@gmail.com>
2024-01-31 18:22:35 +0200

    refactor: add relative class in main

--- client/layouts/default.vue
+++ client/layouts/default.vue
@@ -1,9 +1,9 @@
 <script setup lang="ts"></script>
 
 <template>
-  <div class="py-10 lg:flex gap-6">
+  <div class="py-10 lg:flex gap-6 min-h-screen">
     <app-aside></app-aside>
-    <main class="lg:w-[calc(100%-300px)] ml-auto">
+    <main class="relative lg:w-[calc(100%-300px)] ml-auto">
       <slot></slot>
     </main>
   </div>
