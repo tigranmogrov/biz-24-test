b3a5d7778fc90390289f40a0a36d98b6656ada86
Tigran Mogrov <tigranmogrov@gmail.com>
2024-01-31 18:20:12 +0200

    refactor: add to home page components spiner and product-card

--- client/pages/index.vue
+++ client/pages/index.vue
@@ -1,7 +1,33 @@
-<script setup lang="ts"></script>
+<script setup lang="ts">
+import AppSpinner from '~/components/AppSpinner.vue';
+import { useProductStore } from '~/stores/product';
+
+const productStore = useProductStore();
+const route = useRoute();
+
+onMounted(() => productStore.fetchProducts(route.query));
+
+watch(
+  () => route.query,
+  (newQuery) => {
+    productStore.fetchProducts(newQuery);
+  }
+);
+</script>
 
 <template>
-  <div></div>
+  <div>
+    <app-spinner v-if="productStore.loading" />
+
+    <div v-else>
+      <ul class="grid grid-cols-1 md:grid-cols-2 xl:grid-cols-3 gap-4">
+        <li v-for="product in productStore.getProducts" :key="product._id" class="block">
+          <app-product-card :product="product" />
+        </li>
+      </ul>
+      <div v-if="!productStore.getProducts.length"><strong>Products is empty...</strong></div>
+    </div>
+  </div>
 </template>
 
 <style scoped></style>
