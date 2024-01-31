e7147c17dcbcbb68d29b2236edc7849ab5db0ab7
Tigran Mogrov <tigranmogrov@gmail.com>
2024-01-31 17:38:24 +0200

    fix: product card router-link name

--- client/components/AppProductCard.vue
+++ client/components/AppProductCard.vue
@@ -8,7 +8,7 @@ defineProps<{
 
 <template>
   <div class="bg-gray-3 border border-gray-200 rounded-lg shadow">
-    <router-link :to="{ name: 'product-slug', params: { slug: product.slug } }" class="block p-5">
+    <router-link :to="{ name: 'products-slug', params: { slug: product.slug } }" class="block p-5">
       <img class="block h-48 w-[100%] object-cover" :src="product.image" :alt="product.name" />
       <h3 class="mt-4">{{ product.name }}</h3>
       <p class="mt-2 font-normal text-gray-700 dark:text-gray-500 line-clamp-2">
