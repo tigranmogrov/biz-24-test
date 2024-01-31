37ea37b3c80824251d866890a96e57297d48d2e0
Tigran Mogrov <tigranmogrov@gmail.com>
2024-01-31 17:42:32 +0200

    feat(layouts): add empty and default

---
+++ client/layouts/default.vue
@@ -0,0 +1,12 @@
+<script setup lang="ts"></script>
+
+<template>
+  <div class="py-10 lg:flex gap-6">
+    <app-aside></app-aside>
+    <main class="lg:w-[calc(100%-300px)] ml-auto">
+      <slot></slot>
+    </main>
+  </div>
+</template>
+
+<style scoped></style>
