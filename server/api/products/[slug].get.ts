b32923330953963fa7ed90e2610ce8ad7f4a0519
Tigran Mogrov <tigranmogrov@gmail.com>
2024-01-31 15:33:56 +0200

    feat(server): rout get product by slug

---
+++ server/api/products/[slug].get.ts
@@ -0,0 +1,23 @@
+import { H3Error, H3Event } from 'h3';
+import { ProductErrorEnum } from '~~/enum/errors.enum';
+import { Product } from '~~/server/api/products/product.model';
+import { IProduct } from '~~/types/product';
+
+export default defineEventHandler(async (event: H3Event): Promise<H3Error | { product: IProduct }> => {
+  const routeSlug = event.context.params?.slug;
+  try {
+    const product = await Product.find({ slug: routeSlug }).exec();
+
+    if (!product.length) {
+      return createError({
+        statusMessage: ProductErrorEnum.PRODUCT_NOT_FOUND,
+        status: 404,
+      });
+    }
+    return { product: product[0] };
+  } catch (error: any) {
+    return createError({
+      message: error.message,
+    });
+  }
+});
